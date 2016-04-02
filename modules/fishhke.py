95a0d2e6c4c2a61c2455ee6090060d5963a144d0
Sergio Uve <sergiouve@gmail.com>
2016-04-02 19:55:06 +0200

    Rude fishhke

--- modules/fishhke.py
+++ modules/fishhke.py
@@ -1,2 +1,15 @@
+import sys
+import os
+
 def fishhke(settings):
-	print 'fishhke'
+	if is_fishh_project():
+		print 'scafold!'
+	else:
+		print 'create!'
+
+def is_fishh_project():
+	is_fishh = False
+
+	'''check for fishh project structure'''
+
+	return is_fishh
