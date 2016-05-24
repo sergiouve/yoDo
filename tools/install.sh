4dd101648cfd15a4a671f1b9739c7a9e96bb6c4a
sergiouve <sergiouve@gmail.com>
2016-05-25 01:00:39 +0200

    Update installation script

--- tools/install.sh
+++ tools/install.sh
@@ -10,13 +10,29 @@ case "$OSTYPE" in
 	linux*)   echo 'export PATH="~/.yodo:$PATH"'  >> ~/.bashrc; source ~/.bashrc; echo 'linux' ;;
 esac
 
+echo 'Checking previous installation...'
+
 if [ -d .yodo ]; then
 	update=true
 	cp ~/.yodo/settings ~/yodo_settings_bk
 	rm -rf ~/.yodo
 fi
 
-git clone https://github.com/sergiouve/yoDo.git
+if [ "$update" = true ]; then
+	echo 'Previous installation found. Updating...'
+else
+	echo 'No previous installation found. Installing...'
+fi
+
+hash git >/dev/null 2>&1 || {
+    echo "Error: git is not installed"
+    exit 1
+}
+
+git clone --depth=1 https://github.com/sergiouve/yoDo.git || {
+	echo 'Error: git clone failed.'
+	exit 1
+}
 mkdir .yodo
 cp yoDo/. .yodo/ -R
 chmod a+x -R .yodo
